f657cb11a768313156c81baf4d1cdd1406522711
Tomas Mulder <tcmulder@gmail.com>
2026-01-19 08:12:39 -0700

    Use gsap for character ani

--- src/util/aniSprite.js
+++ src/util/aniSprite.js
@@ -1,3 +1,5 @@
+import { gsap } from 'gsap';
+
 /**
  * Animate image sprites
  *
@@ -15,12 +17,32 @@ export const createAniSprite = async (els) => {
 		}
 		el.classList.add('is-animating');
 		const children = [...el.children];
-		const interval = el.dataset.sprite;
-		let i = 0;
-		children.forEach((child) => (child.style.visibility = 'hidden'));
-		el.nonStandardTimer = setInterval(() => {
-			children[i].style.visibility = 'hidden';
-			children[(i = (i + 1) % children.length)].style.visibility = 'visible';
-		}, interval);
+		const interval = parseInt(el.dataset.sprite) || 100;
+		const duration = interval / 1000; // Convert ms to seconds
+		
+		// Hide all children initially
+		gsap.set(children, { visibility: 'hidden' });
+		
+		// Create a timeline that cycles through children infinitely
+		const timeline = gsap.timeline({ repeat: -1 });
+		
+		children.forEach((child, index) => {
+			// Hide previous child (or all if first frame)
+			if (index === 0) {
+				timeline.set(children, { visibility: 'hidden' }, 0);
+			} else {
+				timeline.set(children[index - 1], { visibility: 'hidden' }, index * duration);
+			}
+			// Show current child
+			timeline.set(child, { visibility: 'visible' }, index * duration);
+		});
+		
+		// Complete the loop: hide last child and show first child
+		const lastIndex = children.length - 1;
+		timeline.set(children[lastIndex], { visibility: 'hidden' }, lastIndex * duration + duration);
+		timeline.set(children[0], { visibility: 'visible' }, lastIndex * duration + duration);
+		
+		// Store timeline on element for potential cleanup
+		el.gsapTimeline = timeline;
 	});
 };
