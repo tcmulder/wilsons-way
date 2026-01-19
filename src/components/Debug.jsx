5404f383ff34ba3ee9d09adf80082ca70c4fb448
Tomas Mulder <tcmulder@gmail.com>
2026-01-19 11:16:06 -0700

    Improve debug options

--- src/components/Debug.jsx
+++ src/components/Debug.jsx
@@ -1,35 +1,68 @@
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useGameContext } from '../context/useGameContext';
-import { useNavigate } from 'react-router-dom';
 import { doPause, doPlay } from '../util/doMovement';
 
 import '../css/debug.css';
 
-export const Debug = () => {
-	const navigate = useNavigate();
-	const { debug, level, setLevel, character, setCharacter, timelines, status, setStatus } = useGameContext();
+const DebugRestart = () => {
+	return (
+		<button onClick={(e) => { e.preventDefault(); window.location.reload(); }}>✖︎</button>
+	);
+};
 
-	if (!debug) {
+const DebugNavigation = ({navigate}) => {
+	return (
+		<select onChange={(e) => { e.preventDefault(); navigate(e.target.value); }}>
+			<option value="/">Intro</option>
+			<option value="/gameplay">Gameplay</option>
+		</select>
+	);
+}
+
+const DebugPlayPause = ({ character, timelines, status, setStatus, path, navigate, setLevel }) => {
+	if (path === '/gameplay' && status.pause === 'pause') {
+		return <button onClick={(e) => { e.preventDefault(); doPlay({timelines: [...timelines, character.timeline], setStatus}); }}>▶︎</button>
+	} else if (path === '/gameplay' && status.pause === 'none') {
+		return <button onClick={(e) => { e.preventDefault(); doPause({timelines: [...timelines, character.timeline], setStatus}); }}>⏸︎</button>
+	} else {
+		return <button onClick={(e) => { e.preventDefault(); navigate('/gameplay'); setLevel(1); }}>⏯︎</button>
+	}
+};
+
+const DebugLevel = ({ level, setLevel, path }) => {
+	if (path !== '/gameplay') return null;
+	return (
+		<span>
+			<button onClick={(e) => { e.preventDefault(); setLevel(level - 1); }}>←</button>level{level}<button onClick={(e) => { e.preventDefault(); setLevel(level + 1); }}>→</button>
+		</span>
+	);
+};
+
+const DebugCharacter = ({ character, setCharacter, path }) => {
+	if (path !== '/gameplay') return null;
+	return (
+		<span>
+			<button onClick={(e) => { e.preventDefault(); setCharacter({...character, id: character.id - 1}); }}>←</button>char{character.id}<button onClick={(e) => { e.preventDefault(); setCharacter({...character, id: character.id + 1}); }}>→</button>
+		</span>
+	);
+};
+
+export const Debug = () => {
+	const props = useGameContext();
+	const navigate = useNavigate();
+	const path = useLocation().pathname;
+
+	if (!props.debug) {
 		return null;
 	}
 	
 	return (
 		<div className="sr-debug">
-			<button onClick={(e) => { e.preventDefault(); window.location.reload(); }}>✖︎</button>
-			{'pause' === status.pause ? (
-				<>
-					<button onClick={(e) => { e.preventDefault(); doPlay({timelines: [...timelines, character.timeline], setStatus}); }}>▶︎</button>
-					<button onClick={(e) => { e.preventDefault(); navigate('/'); }}>Intro</button>
-					<button onClick={(e) => { e.preventDefault(); navigate('/gameplay'); setLevel(1); }}>Gameplay</button>
-				</>
-			) : (
-				<button onClick={(e) => { e.preventDefault(); doPause({timelines: [...timelines, character.timeline], setStatus}); }}>⏸︎</button>
-			)}
-			<span>
-				<button onClick={(e) => { e.preventDefault(); setLevel(level - 1); }}>←</button>level{level}<button onClick={(e) => { e.preventDefault(); setLevel(level + 1); }}>→</button>
-			</span>
-			<span>
-				<button onClick={(e) => { e.preventDefault(); setCharacter({...character, id: character.id - 1}); }}>←</button>char{character.id}<button onClick={(e) => { e.preventDefault(); setCharacter({...character, id: character.id + 1}); }}>→</button>
-			</span>
+			<DebugNavigation {...props} navigate={navigate} path={path} />
+			<DebugRestart />
+			<DebugPlayPause {...props} path={path} navigate={navigate} />
+			<DebugLevel {...props} path={path} />
+			<DebugCharacter {...props} path={path} />
 		</div>
 	);
 };
