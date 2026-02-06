f6b0ce27cd8d056ca4683bf02099f895eecd0f51
Tomas Mulder <tcmulder@gmail.com>
2026-02-06 08:13:57 -0700

    Expose routes for debugger

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
+import { MemoryRouter, HashRouter } from 'react-router-dom'
 import IntroPage from './routes/IntroPage';
 import LevelPage from './routes/LevelPage';
 import { Interface } from './components/Interface';
 import { Debug } from './components/Debug';
+import { useDebugContext } from './context/useContexts';
 
 export default function App() {
+  const { debug: { router } } = useDebugContext();
+  const Router = router === '1' ? HashRouter : MemoryRouter;
   return (
-    <>
-      <div className="sr">
-        <div className="sr-stage">
-          <div className="sr-typography">
+    <div className="sr">
+      <div className="sr-stage">
+        <div className="sr-typography">
+          <Router>
             <Debug />
             <Interface />
             <Routes>
               <Route path="/" element={<IntroPage />} />
               <Route path="/gameplay" element={<LevelPage />} />
             </Routes>
-          </div> 
-        </div>
+          </Router>
+        </div> 
       </div>
-    </>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { MemoryRouter } from 'react-router-dom'
 import './css/global.css'
 import App from './App.jsx'
 import { AppProviders } from './context/AppProviders'
@@ -8,9 +7,7 @@ import { AppProviders } from './context/AppProviders'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProviders>
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
+      <App />
     </AppProviders>
   </StrictMode>,
-)
+);
