588f70e91e2ba1e954a54c698f99992529ff1e4a
Tomas Mulder <tcmulder@gmail.com>
2026-02-26 07:59:54 -0700

    Refactor Gameplay.jsx

--- src/components/Gameplay.jsx
+++ src/components/Gameplay.jsx
@@ -3,13 +3,16 @@ import { useSetupGameplayElevations } from '../hooks/useSetupGameplayElevations'
 import { useMovementTicker } from '../hooks/useMovementTicker';
 
 /**
- * Orchestrates gameplay hooks: element refs, elevations/jump, and movement ticker. Renders nothing.
+ * Orchestrates gameplay hooks: element refs, elevations, and movement ticker. Renders nothing.
+ * 
+ * @param {Object} props The properties object
+ * @param {React.RefObject} props.boardRef Reference to the board element
+ * @returns {React.ReactNode} The Gameplay component.
  */
 const Gameplay = ({ boardRef }) => {
 	useSetupGameplayElements(boardRef);
 	useSetupGameplayElevations();
 	useMovementTicker();
-
 	return null;
 };
 
