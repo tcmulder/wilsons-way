9008aee1d9b106726098464db00b8665d9364ea2
Tomas Mulder <tcmulder@gmail.com>
2026-03-12 07:03:27 -0600

    Fix next level loader

--- src/components/Outro.jsx
+++ src/components/Outro.jsx
@@ -15,17 +15,14 @@ const Outro = (props) => {
 
 	// Auto-navigate to transition page
 	useEffect(() => {
-		timedNavigate({ route: `/level/${levelNumber}/transition`, delay: 3000 });
+		timedNavigate({ route: `/level/${levelNumber + 1}/transition`, delay: 3000 });
 	}, [levelNumber, timedNavigate]);
 
 	return (
 		<>
 			<h1>You've completed Level {levelNumber}!</h1>
-			<Message messageKey={`level_${levelNumber}_outro`} />
 			<p>Your score and stuff will be shown here</p>
-			<Link to={`/level/${levelNumber + 1}`}>
-				Load Level {levelNumber + 1} &gt;&gt;
-			</Link>
+			<Message messageKey={`level_${levelNumber}_outro`} />
 		</>
 	);
 };
