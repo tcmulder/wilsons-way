c3ddae81e14f5032c9c7d82bdb632d13d60721a4
Tomas Mulder <tcmulder@gmail.com>
2026-04-04 12:18:44 -0600

    Animate messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { gsap } from 'gsap';
+import { SplitText } from 'gsap/SplitText';
 import '../css/message.css';
+
+gsap.registerPlugin(SplitText);
 
 /**
  * Message component
- * 
+ *
  * @param {Object} props The properties object
  * @param {string} props.messageKey The key of the message to display
  * @returns {React.ReactNode} The Message component
@@ -11,15 +15,45 @@ import '../css/message.css';
 const Message = (props) => {
   const { messageKey } = props;
   const [message, setMessage] = useState('');
+  const containerRef = useRef(null);
+
   useEffect(() => {
     fetch(`${window.sr.api}shelf-runner/v1/message/${messageKey}`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setMessage(data.data.value);
       });
   }, [messageKey]);
-  return <div className="sr-message" dangerouslySetInnerHTML={{ __html: message }} />;
-};
 
+  useLayoutEffect(() => {
+    const el = containerRef.current;
+    if (!message || !el) return undefined;
+
+    const split = SplitText.create(el, {
+      type: 'chars, words',
+      onSplit(self) {
+        return gsap.from(self.chars, {
+          autoAlpha: 0,
+          duration: 0.001,
+          stagger: 0.005,
+          ease: 'none',
+        });
+      },
+    });
+
+    return () => {
+      gsap.killTweensOf(split.chars);
+      split.kill();
+    };
+  }, [message]);
+
+  return (
+    <div
+      ref={containerRef}
+      className="sr-message"
+      dangerouslySetInnerHTML={{ __html: message }}
+    />
+  );
+};
 
 export default Message;
