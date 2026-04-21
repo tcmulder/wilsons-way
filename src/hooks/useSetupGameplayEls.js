94cf56fe1ff2456119aa33f22045d6ff846ce820
Tomas Mulder <tcmulder@gmail.com>
2026-04-21 15:56:18 -0600

    Remove console log

--- src/hooks/useSetupGameplayEls.js
+++ src/hooks/useSetupGameplayEls.js
@@ -88,9 +88,7 @@ export function useSetupGameplayElements(boardRef) {
 				// Run fallback so ALL shelves and obstacles are used to track collisions (albeit inefficiently).
 				if (firstRun) {
 					firstRun = false;
-					console.log('👁️', shelves.length, elShelvesVisible.size);
 					if (shelves.length > 0 && (elShelvesVisible.size === 0 || elShelvesVisible.size === shelves.length)) {
-						console.log('👀', shelves.length, elShelvesVisible.size);
 						shelvesObserver.disconnect();
 						obstaclesObserver.disconnect();
 						shelves.forEach((x) => elShelvesVisible.add(x));
